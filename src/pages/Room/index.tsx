346b863ef35cbf56d8abbd133532f9423a79c68b
Eldad <eldad@spot.im>
2020-04-18 17:31:21 +0300

    clean up

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,40 +1,14 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import { useTranslation } from 'react-i18next';
-import gsap, { Back } from 'gsap';
-import * as youtube from '../../clients/youtube';
-import debounce from 'lodash.debounce';
-import firebase from 'firebase/app';
-import { useHistory, useRouteMatch } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import YouTubePlayer from '../../components/YouTubePlayer';
 import { ReactComponent as GamepadIcon } from '../../svgs/gamepad.svg';
-import { IVideo, TPlatform, IRoom } from '../../types';
+import { IRoom } from '../../types';
 import { roomDocRef } from '../../firestore-refs';
-
-type TMode = 'home' | 'search';
 
 interface IRoomParams {
   roomId: string;
-}
-
-function parseVideoId(
-  videoId: string
-): { platform: TPlatform; videoId: string } {
-  const [platformId, id] = videoId.split('_');
-
-  let platform: TPlatform;
-  if (platformId === 'yt') {
-    platform = 'youtube';
-  } else if (platformId === 'vm') {
-    platform = 'vimeo';
-  } else {
-    platform = 'redtube';
-  }
-
-  return {
-    platform,
-    videoId: id,
-  };
 }
 
 export default () => {
